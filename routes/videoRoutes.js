6ead32de1d3825baa934529ade39d17506f1b163
Shittu Emmanuel Seun <shem4soul@gmail.com>
2025-07-01 20:46:19 +0100

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -4,7 +4,6 @@ const express = require("express")
 const router = express.Router()
 const { downloadVideo } = require("../controllers/videoController")
 
-// Route to download the video
 router.get("/download", downloadVideo)
 
 module.exports = router
